261f0717f446b08249cef6ae756d2349047a9b8a
Taylor Ackley <taylora@knowledgeanywhere.com>
2021-07-21 20:05:34 -0700

    changes

--- apps/activity-feed/src/app/core/components/posts/post/post.component.ts
+++ apps/activity-feed/src/app/core/components/posts/post/post.component.ts
@@ -2,6 +2,7 @@ import { IPost, LinkRel } from '@activity-feed/api-interfaces';
 import { Component, Input, OnInit } from '@angular/core';
 import { LinksService } from '../../../services/links.service';
 import { first } from 'rxjs/operators';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'activity-feed-post',
@@ -9,16 +10,15 @@ import { first } from 'rxjs/operators';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+  faEllipsisV: faEllipsisV;
   @Input() post!: IPost;
   constructor(private linksService: LinksService) { }
 
   ngOnInit(): void {
-    console.log(this.post);
   }
 
   refetchSelf() {
     const link = this.post?.links?.find(link => link.rel === LinkRel.self);
-    console.log(link);
     if (link) {
       this.linksService.dispatch(link).pipe(first()).subscribe((post: IPost) => this.post = post);
     }
